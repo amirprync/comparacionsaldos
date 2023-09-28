c4097f5309a2306696570cf75f3cab5ebb1161e4
amirprync <60889238+amirprync@users.noreply.github.com>
2023-09-27 22:50:17 -0300

    Update App.py

--- App.py
+++ App.py
@@ -25,20 +25,30 @@ def compare_files(file1, file2):
         st.error("El archivo 2 debe tener columnas llamadas 'numero de comitente' y 'cantidad'")
         return
     
-    merged_df = pd.merge(df1, df2, on='numero de comitente', how='outer', suffixes=('_file1', '_file2'))
+    merged_df = pd.merge(df1, df2, on='numero de comitente', how='outer', suffixes=('_nasdaq', '_BO'))
+    merged_df['diferencia'] = merged_df['cantidad_nasdaq'] - merged_df['cantidad_BO']
     
-    diff_df = merged_df[merged_df['cantidad_file1'] != merged_df['cantidad_file2']]
-    
-    if diff_df.empty:
+    if merged_df['diferencia'].isna().all():
         st.write("No hay diferencias en las cantidades para los mismos números de comitente.")
     else:
         st.write("Se encontraron diferencias en las siguientes entradas:")
-        st.write(diff_df)
+        st.write(merged_df)
+        
+        # Permitir la descarga en Excel
+        excel_io = io.BytesIO()
+        with pd.ExcelWriter(excel_io, engine='xlsxwriter') as writer:
+            merged_df.to_excel(writer, sheet_name='Resultados', index=False)
+        excel_io.seek(0)
+        st.download_button(
+            label="Descargar resultados en Excel",
+            data=excel_io,
+            file_name="resultados_comparacion.xlsx",
+            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        )
 
 # Streamlit app
 st.title("Comparador de Archivos Excel")
 
-# Botón para descargar el archivo Excel modelo
 sample_excel = create_sample_excel()
 st.download_button(
     label="Descargar archivo Excel modelo",
@@ -47,8 +57,8 @@ st.download_button(
     mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 )
 
-uploaded_file1 = st.file_uploader("Sube el primer archivo Excel", type=['xlsx'])
-uploaded_file2 = st.file_uploader("Sube el segundo archivo Excel", type=['xlsx'])
+uploaded_file1 = st.file_uploader("Sube el archivo Excel para NASDAQ", type=['xlsx'])
+uploaded_file2 = st.file_uploader("Sube el archivo Excel para BO", type=['xlsx'])
 
 if uploaded_file1 is not None and uploaded_file2 is not None:
     compare_files(uploaded_file1, uploaded_file2)
